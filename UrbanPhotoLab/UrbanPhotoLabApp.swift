fc328039d8a154bff43b1ed9018dcf1d230653ff
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-10-26 14:35:23 +0800

    Initial Commit

---
+++ UrbanPhotoLab/UrbanPhotoLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanPhotoLabApp.swift
+//  UrbanPhotoLab
+//
+//  Created by Andriyanto Halim on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct UrbanPhotoLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UrbanPhotoLabTests/UrbanPhotoLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanPhotoLabTests.swift
+//  UrbanPhotoLabTests
+//
+//  Created by Andriyanto Halim on 26/10/24.
+//
+
+import Testing
+@testable import UrbanPhotoLab
+
+struct UrbanPhotoLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
